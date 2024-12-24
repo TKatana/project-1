028c835fcdae191a07452c0036472171b619c9e7
TKatana <turjohalder41@gmail.com>
2024-12-24 21:47:25 +0600

    after coppying website to flask app

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-from flask import Flask
-from handlers.routes import configure_routes
+from website import create_app
 
-app = Flask(__name__)
+app = create_app()
 
-configure_routes(app)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
